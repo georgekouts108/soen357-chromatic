fb944beb7416bddb09b76e0a960859a384afacec
hackerrank <59776984+georgekouts108@users.noreply.github.com>
2022-03-21 23:14:35 -0400

    #1 added enum for Genres

---
+++ Genre.py
@@ -0,0 +1,28 @@
+import enum
+
+
+class Genre(enum.Enum):
+    POP = 'POP',
+    ELECTRONIC = 'ELECTRONIC',
+    DANCE = 'DANCE',
+    RAP = 'RAP',
+    HIPHOP = 'HIPHOP',
+    ROCK = 'ROCK',
+    METAL = 'METAL',
+    SOUL = 'SOUL',
+    JAZZ = 'JAZZ',
+    DISCO = 'DISCO',
+    FUNK = 'FUNK',
+    CLASSICAL = 'CLASSICAL',
+    SOUNDTRACK = 'SOUNDTRACK',
+    WORLD = 'WORLD',
+    FOLK = 'FOLK',
+    INDIE = 'INDIE',
+    BROADWAY = 'BROADWAY',
+    THEATER = 'THEATER',
+    MUSICAL = 'MUSICAL',
+    VINTAGE = 'VINTAGE',
+    BALLAD = 'BALLAD',
+    RETRO = 'RETRO',
+    MEDITATION = 'MEDITATION',
+    FAITH = 'FAITH'
